08ed44e24b4211ebd420a677f12c44c2cf5ae26f
ChanningKuo <492689363@qq.com>
2020-08-11 20:35:50 +0800

    initial structure frame

---
+++ Super Mario Swift Shared/Components/Enemy.swift
@@ -0,0 +1,9 @@
+//
+//  Enemy.swift
+//  Super Mario Swift
+//
+//  Created by Channing Kuo on 2020/8/11.
+//  Copyright © 2020 Channing Kuo. All rights reserved.
+//
+
+import Foundation
---
+++ Super Mario Swift Shared/Setup.swift
@@ -0,0 +1,9 @@
+//
+//  Setup.swift
+//  Super Mario Swift
+//
+//  Created by Channing Kuo on 2020/8/11.
+//  Copyright © 2020 Channing Kuo. All rights reserved.
+//
+
+import Foundation
