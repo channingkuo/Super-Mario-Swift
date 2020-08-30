d11487985405bffc4b30843087b3ea0942134e88
ChanningKuo <492689363@qq.com>
2020-08-30 18:32:01 +0800

    draw map ground done

--- Super Mario Swift Shared/Components/Map.swift
+++ Super Mario Swift Shared/Components/Map.swift
@@ -30,19 +30,59 @@ class Map: SKScene {
 
         let col = elementValue(element: map, forKey: "col", type: CGFloat.self)
         let row = elementValue(element: map, forKey: "row", type: CGFloat.self)
-        let height = elementValue(element: map, forKey: "height", type: CGFloat.self)
         let baseWidth = elementValue(element: identity, forKey: "width", type: CGFloat.self)
         let baseHeight = elementValue(element: identity, forKey: "height", type: CGFloat.self)
 
-        self.size = CGSize(width: col * baseWidth, height: height)
+        self.size = CGSize(width: Constants.W_SCREEN, height: Constants.H_SCREEN)
+        self.scaleMode = .aspectFill
+        
 
-        let ground = elementValue(element: self.mapDictionary, forKey: "ground", type: NSArray.self)
-
-        let brickTileSet = SKTileSet(named: "BrickTileSet")!
-        let tileMap = SKTileMapNode(tileSet: brickTileSet, columns: Int(col), rows: Int(ceil(row)), tileSize: CGSize(width: baseWidth, height: baseHeight), tileGroupLayout: brickTileSet.tileGroups)
+        guard let groundBrickTileSet = SKTileSet(named: "GroundBrickTileSet") else {
+          fatalError("GroundBrickTileSet Tile Set not found")
+        }
+        
+        let columns = Int(ceil(col))
+        let rows = Int(ceil(row))
+        
+        let tileMap = SKTileMapNode(tileSet: groundBrickTileSet, columns: columns, rows: rows, tileSize: CGSize(width: baseWidth, height: baseHeight))
+        tileMap.position = CGPoint(x: 0, y: 0)
         tileMap.anchorPoint = CGPoint(x: 0, y: 0)
-        tileMap.position = CGPoint(x: 300, y: 0)
         self.addChild(tileMap)
+        
+        let groundBrickTileGroup = groundBrickTileSet.tileGroups
+        
+        guard let groundBrickTile = groundBrickTileGroup.first(where: {$0.name == "CenterBrick"}) else {
+            fatalError("No GroundBrick Group found")
+        }
+        
+        guard let groundUpperEdgeBrickTile = groundBrickTileGroup.first(where: {$0.name == "UpperEdgeBrick"}) else {
+            fatalError("No UpperEdgeBrick Group found")
+        }
+        
+        let ground = elementValue(element: self.mapDictionary, forKey: "ground", type: NSArray.self)
+        // 计算ground镂空部分
+        var fullColums: [Int] = [Int]()
+        for item in ground {
+            let itemDic = item as! NSDictionary
+            let colValue = elementValue(element: itemDic, forKey: "col", type: Int.self)
+            let stepValue = elementValue(element: itemDic, forKey: "step", type: Int.self)
+            for index in 1...stepValue {
+                fullColums.append(colValue + index - 1)
+            }
+        }
+        
+        // 动态绘制地板瓦片精灵
+        for row in 0...1 {
+            if row == 0 {
+                tileMap.tileSize = CGSize(width: 55, height: 39)
+            }
+            for col in 0...columns {
+                if fullColums.contains(col) {
+                    continue
+                }
+                tileMap.setTileGroup(row == 0 ? groundUpperEdgeBrickTile : groundBrickTile, forColumn: col, row: row)
+            }
+        }
     }
     
     func elementValue<T>(element: NSDictionary, forKey key: String, type: T.Type) -> T {
