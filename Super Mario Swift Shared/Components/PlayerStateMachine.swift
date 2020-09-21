c32cce4e62f483ed1e26df93b7c179a4a02605f2
ChanningKuo <492689363@qq.com>
2020-09-21 19:57:35 +0800

    state machine change

--- Super Mario Swift Shared/Components/PlayerStateMachine.swift
+++ Super Mario Swift Shared/Components/PlayerStateMachine.swift
@@ -21,6 +21,41 @@ class PlayerState: GKState {
     }
 }
 
+class IdleState: PlayerState {
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        switch stateClass {
+        case is SlowingState.Type:
+            return false
+        default:
+            return true
+        }
+    }
+    
+    let texture = SKTexture(imageNamed: "player_type_1_1")
+    lazy var action = { SKAction.animate(with: [texture], timePerFrame: 0.1) }()
+    
+    override func didEnter(from previousState: GKState?) {
+        player.removeAction(forKey: characterAnimationKey)
+        player.run(action, withKey: characterAnimationKey)
+    }
+}
+
+class WalkingState: PlayerState {
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        return true
+    }
+    
+    let textures: Array<SKTexture> = (1..<5).map({ return "player_type_1_\($0)" }).map(SKTexture.init)
+    lazy var action = { SKAction.repeatForever(.animate(with: textures, timePerFrame: 0.1)) }()
+    
+    override func didEnter(from previousState: GKState?) {
+        player.removeAction(forKey: characterAnimationKey)
+        player.run(action, withKey: characterAnimationKey)
+    }
+}
+
 class JumpingState: PlayerState {
     var hasFinishedJump: Bool = false
     
@@ -37,6 +72,7 @@ class JumpingState: PlayerState {
         player.run(action, withKey: characterAnimationKey)
         
         hasFinishedJump = false
+        // TODO 跳起来的位置变换处理，曲线运动，空中受方向键影响的处理
         player.run(.move(by: CGVector(dx: 0.0, dy: 110), duration: 0.1))
         
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) {(timer) in
@@ -45,62 +81,25 @@ class JumpingState: PlayerState {
     }
 }
 
-class LandingState: PlayerState {
-    
-    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
-        switch stateClass {
-        case is LandingState.Type, is JumpingState.Type, is SlowingState.Type:
-            return false
-        default:
-            return true
-        }
-    }
-    
-    override func didEnter(from previousState: GKState?) {
-        stateMachine?.enter(IdleState.self)
-    }
-}
-
-class WalkingState: PlayerState {
-    
-    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
-        switch stateClass {
-        case is LandingState.Type, is WalkingState.Type, is SlowingState.Type:
-            return false
-        default:
-            return true
-        }
-    }
-    
-    let textures: Array<SKTexture> = (1..<5).map({ return "player_type_1_\($0)" }).map(SKTexture.init)
-    lazy var action = { SKAction.repeatForever(.animate(with: textures, timePerFrame: 0.1)) }()
-    
-    override func didEnter(from previousState: GKState?) {
-        player.removeAction(forKey: characterAnimationKey)
-        player.run(action, withKey: characterAnimationKey)
-    }
-}
-
-class IdleState: PlayerState {
-    
-    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
-        switch stateClass {
-        case is LandingState.Type, is SlowingState.Type:
-            return false
-        default:
-            return true
-        }
-    }
-    
-    let texture = SKTexture(imageNamed: "player_type_1_1")
-    lazy var action = { SKAction.animate(with: [texture], timePerFrame: 0.1) }()
-    
-    override func didEnter(from previousState: GKState?) {
-        player.removeAction(forKey: characterAnimationKey)
-        player.run(action, withKey: characterAnimationKey)
-    }
-}
-
 class SlowingState: PlayerState {
     
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        if stateClass is IdleState.Type { return false }
+        return true
+    }
+    
+    override func didEnter(from previousState: GKState?) {
+        
+    }
+}
+
+class DeadState: PlayerState {
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        return false
+    }
+    
+    override func didEnter(from previousState: GKState?) {
+        // TODO dead enter next game or game over
+    }
 }
